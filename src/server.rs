ef1bd52909dad3b50cdda9038bb6ecba79f91465
Koen Wilde <koen@chillheid.nl>
2018-06-20 22:30:47 +0200

    Handle request in separate method

--- src/server.rs
+++ src/server.rs
@@ -2,20 +2,24 @@ extern crate std;
 extern crate tokio;
 
 use self::tokio::io;
-use self::tokio::net::TcpListener;
+use self::tokio::net::{TcpListener, TcpStream};
 use self::tokio::prelude::*;
+
+fn process_request(socket: TcpStream) {
+    let task = io::write_all(socket, "Hello, world!\n")
+        .then(|res| {
+            println!("handled request, result: {:?}", res);
+            Ok(())
+        });
+    tokio::spawn(task);
+}
 
 pub fn listen() {
     let addr = "127.0.0.1:8080".parse().unwrap();
     let listener = TcpListener::bind(&addr).unwrap();
 
     let server = listener.incoming().for_each(|socket| {
-        let task = io::write_all(socket, "hello world\n")
-            .then(|res| {
-                println!("wrote some message to some socket, result: {:?}", res);
-                Ok(())
-            });
-        tokio::spawn(task);
+        process_request(socket);
         Ok(())
     })
         .map_err(|err| println!("got some error: {:?}", err));
