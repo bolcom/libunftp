16138fc3f9643d7e3d25b9723adc28a05f485503
Dávid Kosztka <dkosztka@bol.com>
2019-08-30 16:49:58 +0200

    reformat

--- examples/gcs.rs
+++ examples/gcs.rs
@@ -6,8 +6,10 @@ pub fn main() {
     let addr = "127.0.0.1:8080";
 
     let server = libunftp::Server::new(Box::new(move || {
-        libunftp::storage::cloud_storage::CloudStorage::new("bolcom-dev-unftp-dev-738-unftp-dev", yup_oauth2::service_account_key_from_file(&"/Users/dkosztka/Downloads/bolcom-dev-unftp-dev-738-1379d4070948.json".to_string())
-                .expect("borked"))
+        libunftp::storage::cloud_storage::CloudStorage::new(
+            "bolcom-dev-unftp-dev-738-unftp-dev",
+            yup_oauth2::service_account_key_from_file(&"/Users/dkosztka/Downloads/bolcom-dev-unftp-dev-738-1379d4070948.json".to_string()).expect("borked"),
+        )
     }));
 
     info!("Starting ftp server on {}", addr);
