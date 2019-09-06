b46cb13aee36c208223ae271c97d8d27e0cde27e
Imre Dávid Kosztka <949894+Nonius@users.noreply.github.com>
2019-09-06 15:42:55 +0200

    use yup_oauth2 version 3 (#23)
    * use yup_oauth2 version 3
    
    * reformat
    
    * use only one service account access
    
    * pin yup-oauth2 version

--- examples/gcs.rs
+++ examples/gcs.rs
@@ -1,23 +1,17 @@
-use log::*;
+use log::info;
 
 pub fn main() {
     pretty_env_logger::init();
 
     let addr = "127.0.0.1:8080";
 
-    let server = libunftp::Server::new(Box::new(move || libunftp::storage::cloud_storage::CloudStorage::new("your-bucket-name", Tp {})));
+    let server = libunftp::Server::new(Box::new(move || {
+        libunftp::storage::cloud_storage::CloudStorage::new(
+            "bolcom-dev-unftp-dev-738-unftp-dev",
+            yup_oauth2::service_account_key_from_file(&"/Users/dkosztka/Downloads/bolcom-dev-unftp-dev-738-1379d4070948.json".to_string()).expect("borked"),
+        )
+    }));
 
     info!("Starting ftp server on {}", addr);
     server.listen(addr);
-}
-
-struct Tp {}
-
-impl libunftp::storage::cloud_storage::TokenProvider for Tp {
-    fn get_token(&self) -> std::result::Result<libunftp::storage::cloud_storage::Token, Box<dyn std::error::Error>> {
-        let (token_type, access_token) = sync_oauth2::get_token(
-            sync_oauth2::yup_oauth2::service_account_key_from_file(&"/path/to/your/service/account/key.json".to_string()).expect("borked"),
-        )?;
-        Ok(libunftp::storage::cloud_storage::Token { token_type, access_token })
-    }
 }
