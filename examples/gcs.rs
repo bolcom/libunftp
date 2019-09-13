68be3b5f1081a34335c8e44e2932ff7dbc1a53b3
Agoston Horvath <ahorvath@bol.com>
2019-09-13 16:28:17 +0200

    fixed gcs example (3-way merge f*up)

--- examples/gcs.rs
+++ examples/gcs.rs
@@ -5,7 +5,12 @@ pub fn main() {
 
     let addr = "127.0.0.1:8080";
 
-    let server = libunftp::Server::new(Box::new(move || libunftp::storage::cloud_storage::CloudStorage::new("your-bucket-name", Tp {})));
+    let server = libunftp::Server::new(Box::new(move || {
+        libunftp::storage::cloud_storage::CloudStorage::new(
+            "bolcom-dev-unftp-dev-738-unftp-dev",
+            yup_oauth2::service_account_key_from_file(&"/Users/dkosztka/Downloads/bolcom-dev-unftp-dev-738-1379d4070948.json".to_string()).expect("borked"),
+        )
+    }));
 
     info!("Starting ftp server on {}", addr);
     tokio::run(server.listen(addr));
