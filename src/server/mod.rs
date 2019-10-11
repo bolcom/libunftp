8b91454db6b0d1de48e0a54ff51351b73e1d110c
Rob <rkleingunnewiek@bol.com>
2019-10-11 14:46:13 +0200

    break out of loop upon successful bind

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -427,20 +427,21 @@ where
 
                             let mut listener: Option<std::net::TcpListener> = None;
                             for _ in 1..BIND_RETRIES {
-                                let i = rng.gen_range(0, passive_addrs.len()-1);
+                                let i = rng.gen_range(0, passive_addrs.len() - 1);
                                 match std::net::TcpListener::bind(passive_addrs[i]) {
                                     Ok(x) => {
                                         listener = Some(x);
+                                        break;
                                     }
                                     Err(_) => continue,
                                 };
-                            };
-
-                            if listener.is_none() {
-                                return Ok(Reply::new(ReplyCode::CantOpenDataConnection, "No data connection established"));
                             }
 
-                            let listener = listener.unwrap();
+                            let listener = match listener {
+                                None => return Ok(Reply::new(ReplyCode::CantOpenDataConnection, "No data connection established")),
+                                Some(l) => l,
+                            };
+
                             let addr = match listener.local_addr()? {
                                 std::net::SocketAddr::V4(addr) => addr,
                                 std::net::SocketAddr::V6(_) => panic!("we only listen on ipv4, so this shouldn't happen"),
