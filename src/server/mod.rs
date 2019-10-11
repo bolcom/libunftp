e07a32c221a86620786c5853e3918f98eaea054d
Rob klein Gunnewiek <robklg@users.noreply.github.com>
2019-10-11 15:44:15 +0200

    Support randomized passive listening ports (#42)
    * Support randomized passive listening ports
    
    * rand dependency for port randomization
    
    * break out of loop upon successful bind

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -46,8 +46,12 @@ use crate::storage;
 use crate::storage::filesystem::Filesystem;
 use session::{Session, SessionState};
 
+use rand::Rng;
+
 const DEFAULT_GREETING: &str = "Welcome to the libunftp FTP server";
 const CONTROL_CHANNEL_ID: u8 = 0;
+
+const BIND_RETRIES: u8 = 10;
 
 impl From<commands::ParseError> for FTPError {
     fn from(err: commands::ParseError) -> FTPError {
@@ -435,7 +439,25 @@ where
                         Command::Pasv => {
                             ensure_authenticated!();
 
-                            let listener = std::net::TcpListener::bind(&passive_addrs.as_slice())?;
+                            let mut rng = rand::thread_rng();
+
+                            let mut listener: Option<std::net::TcpListener> = None;
+                            for _ in 1..BIND_RETRIES {
+                                let i = rng.gen_range(0, passive_addrs.len() - 1);
+                                match std::net::TcpListener::bind(passive_addrs[i]) {
+                                    Ok(x) => {
+                                        listener = Some(x);
+                                        break;
+                                    }
+                                    Err(_) => continue,
+                                };
+                            }
+
+                            let listener = match listener {
+                                None => return Ok(Reply::new(ReplyCode::CantOpenDataConnection, "No data connection established")),
+                                Some(l) => l,
+                            };
+
                             let addr = match listener.local_addr()? {
                                 std::net::SocketAddr::V4(addr) => addr,
                                 std::net::SocketAddr::V6(_) => panic!("we only listen on ipv4, so this shouldn't happen"),
