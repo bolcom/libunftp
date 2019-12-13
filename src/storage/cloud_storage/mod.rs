e1739013af45b116907e9e710af519da4773a2ee
Werner Hofstra <wernerhofstra@bol.com>
2019-12-13 16:49:08 +0100

    Add http status code error checking to list (#101)
    * Reduce level of indentation by 1
    
    * Replace map_or with unwrap_or
    
    * Add http status code error checking to list

--- src/storage/cloud_storage/mod.rs
+++ src/storage/cloud_storage/mod.rs
@@ -277,17 +277,20 @@ impl<U: Send> StorageBackend<U> for CloudStorage {
                     .body(Body::empty())
                     .map_err(|_| Error::from(ErrorKind::PermanentFileNotAvailable))
             })
-            .and_then(move |request| {
-                client
-                    .request(request)
+            .and_then(move |request| client.request(request).map_err(|_| Error::from(ErrorKind::PermanentFileNotAvailable)))
+            .and_then(|response| {
+                let status = response.status();
+                response
+                    .into_body()
                     .map_err(|_| Error::from(ErrorKind::PermanentFileNotAvailable))
-                    .and_then(|response| response.into_body().map_err(|_| Error::from(ErrorKind::PermanentFileNotAvailable)).concat2())
+                    .concat2()
+                    .and_then(move |body_string| lift_errors(body_string, status).map_err(|_| Error::from(ErrorKind::PermanentFileNotAvailable)))
                     .and_then(|body_string| {
                         serde_json::from_slice::<ResponseBody>(&body_string)
                             .map_err(|_| Error::from(ErrorKind::PermanentFileNotAvailable))
                             .map(|response_body| {
                                 //TODO: map prefixes
-                                stream::iter_ok(response_body.items.map_or(vec![], |items| items))
+                                stream::iter_ok(response_body.items.unwrap_or(vec![]))
                             })
                     })
             })
