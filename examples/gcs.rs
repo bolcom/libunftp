7205f372458afcc9e99ae93071b22c71ee10bacb
Hannes de Jager <hdejager@bol.com>
2020-03-19 22:31:33 +0100

    Fixes for examples.

--- examples/gcs.rs
+++ examples/gcs.rs
@@ -3,7 +3,7 @@ use log::*;
 pub fn main() {
     pretty_env_logger::init();
 
-    let addr = "127.0.0.1:8080";
+    let addr = "127.0.0.1:2121";
 
     let server = libunftp::Server::new(Box::new(move || {
         libunftp::storage::cloud_storage::CloudStorage::new(
