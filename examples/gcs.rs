3c999b7da7c16b8148bd82f2a6c0fc26ef184c7e
Dávid Kosztka <949894+Nonius@users.noreply.github.com>
2020-04-03 17:47:49 +0200

    remove unwrap

--- examples/gcs.rs
+++ examples/gcs.rs
@@ -5,11 +5,9 @@ pub fn main() -> std::io::Result<()> {
 
     let addr = "127.0.0.1:2121";
 
-    let mut runtime = tokio_compat::runtime::Builder::new().build().unwrap();
+    let mut runtime = tokio_compat::runtime::Builder::new().build()?;
 
-    let service_account_key = runtime.block_on_std(yup_oauth2::read_service_account_key(
-        &"key.json".to_string(),
-    ))?;
+    let service_account_key = runtime.block_on_std(yup_oauth2::read_service_account_key(&"key.json".to_string()))?;
 
     let server = libunftp::Server::new(Box::new(move || {
         libunftp::storage::cloud_storage::CloudStorage::new("my-bucket", service_account_key.clone())
