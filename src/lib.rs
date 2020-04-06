ca54317f98c5bd9e765a3cc3711001473a01321d
Hannes de Jager <hdejager@bol.com>
2020-04-06 09:34:27 +0200

    Make clippy happy

--- src/lib.rs
+++ src/lib.rs
@@ -11,18 +11,12 @@
 //! # Quick Start
 //!
 //! ```rust
-//! use libunftp;
-//! use tokio::prelude::*;
+//!  let ftp_home = std::env::temp_dir();
+//!  let server = libunftp::Server::with_root(ftp_home)
+//!    .greeting("Welcome to my FTP server")
+//!    .passive_ports(50000..65535);
 //!
-//! #[tokio::main]
-//! pub async fn main() {
-//!     let ftp_home = std::env::temp_dir();
-//!     let server = libunftp::Server::with_root(ftp_home)
-//!         .greeting("Welcome to my FTP server")
-//!         .passive_ports(50000..65535);
-//!
-//!     server.listener("127.0.0.1:2121");
-//! }
+//!  server.listener("127.0.0.1:2121");
 //! ```
 
 pub mod auth;
