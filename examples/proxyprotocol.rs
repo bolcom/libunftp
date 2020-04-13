9c222a84d8802219350f86b390a18f0e64fd7a37
Rob <rkleingunnewiek@bol.com>
2020-04-13 23:36:30 +0200

    introduce separate listen method for proxy protocol mode

--- examples/proxyprotocol.rs
+++ examples/proxyprotocol.rs
@@ -6,8 +6,10 @@ pub async fn main() {
     pretty_env_logger::init();
 
     let addr = "127.0.0.1:2121";
-    let server = libunftp::Server::with_root(std::env::temp_dir()).proxy_protocol_mode("10.0.0.1", 2121).unwrap();
+    let server = libunftp::Server::new_with_fs_root(std::env::temp_dir())
+        .proxy_protocol_mode("10.0.0.1", 2121)
+        .unwrap();
 
     info!("Starting ftp server with proxy protocol on {}", addr);
-    server.listener(addr).await;
+    server.listen_proxy_protocol_mode(addr).await;
 }
