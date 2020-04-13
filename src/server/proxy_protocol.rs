6f645178d002cfb49731cc7acd50b9725b38f0b9
Rob <rkleingunnewiek@bol.com>
2020-04-13 21:48:31 +0200

    corrections based on review Hannes

--- src/server/proxy_protocol.rs
+++ src/server/proxy_protocol.rs
@@ -1,8 +1,8 @@
 use bytes::Bytes;
-use tokio::io::AsyncReadExt;
-use proxy_protocol::ProxyHeader;
 use proxy_protocol::version1::ProxyAddressFamily;
+use proxy_protocol::ProxyHeader;
 use std::net::IpAddr;
+use tokio::io::AsyncReadExt;
 
 #[derive(Debug)]
 pub enum ProxyError {
@@ -43,53 +43,59 @@ async fn read_proxy_header(tcp_stream: &mut tokio::net::TcpStream) -> Result<Pro
         match pbuf.iter().position(|b| *b == b'\n') {
             Some(pos) => {
                 // invalid header size
-                if i+pos > rbuf.capacity()
-                    || pos < 13{
-                        return Err(ProxyError::HeaderSize);
+                if i + pos > rbuf.capacity() || pos < 13 {
+                    return Err(ProxyError::HeaderSize);
                 }
 
-                read_half.read(&mut rbuf[i..=i+pos]).await.unwrap();
+                read_half.read(&mut rbuf[i..=i + pos]).await.unwrap();
 
                 // make sure the message ends with crlf or it will panic
-                if rbuf[pos-1] != 0x0d {
+                if rbuf[pos - 1] != 0x0d {
                     return Err(ProxyError::CrlfError);
                 }
 
-                let mut phb = Bytes::copy_from_slice(&rbuf[..=i+pos]);
+                let mut phb = Bytes::copy_from_slice(&rbuf[..=i + pos]);
                 let proxyhdr = match ProxyHeader::decode(&mut phb) {
                     Ok(h) => h,
                     Err(_) => return Err(ProxyError::DecodeError),
                 };
 
                 return Ok(proxyhdr);
-            },
+            }
             None => {
-                if i+n > rbuf.capacity() {
+                if i + n > rbuf.capacity() {
                     return Err(ProxyError::NotProxyHdr);
                 }
 
-                read_half.read(&mut rbuf[i..i+n]).await.unwrap();
+                read_half.read(&mut rbuf[i..i + n]).await.unwrap();
                 i += n;
             }
         }
     }
 }
 
-pub async fn get_peer_with_proxy_protocol(tcp_stream: &mut tokio::net::TcpStream) -> Result<ConnectionTuple, ProxyError> {
+pub async fn get_peer_from_proxy_header(tcp_stream: &mut tokio::net::TcpStream) -> Result<ConnectionTuple, ProxyError> {
     let proxyhdr = match read_proxy_header(tcp_stream).await {
         Ok(v) => v,
         Err(e) => {
             return Err(e);
-        },
+        }
     };
     match proxyhdr {
-        ProxyHeader::Version1 { family, source, source_port, destination, destination_port, .. } => {
+        ProxyHeader::Version1 {
+            family,
+            source,
+            source_port,
+            destination,
+            destination_port,
+            ..
+        } => {
             if family == ProxyAddressFamily::IPv4 {
                 Ok(ConnectionTuple::new(source, source_port, destination, destination_port))
             } else {
                 Err(ProxyError::IPv4Required)
             }
-        },
-        _ => { Err(ProxyError::UnsupportedVersion) }
+        }
+        _ => Err(ProxyError::UnsupportedVersion),
     }
 }
