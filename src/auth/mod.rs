7bec4df6b93c0f2c8886f26006383376eab20025
Hannes de Jager <hdejager@bol.com>
2020-04-18 21:42:50 +0200

    Added account_enabled method to UserDetails trait.

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -1,16 +1,17 @@
 #![deny(missing_docs)]
-//! Contains the `Authenticator` trait that is used by the `Server` and its various implementations
-//! to authenticate users.
+//! Contains the `Authenticator` and `UserDetails` traits that are used by various implementations
+//! and also the `Server` to authenticate users.
 //!
 //! Defines the common interface that can be implemented for a multitude of authentication
 //! backends, e.g. *LDAP* or *PAM*. It is used by [`Server`] to authenticate users.
 //!
-//! You can define your own implementation to integrate the FTP server with whatever authentication
-//! mechanism you need. For example, to define an `Authenticator` that will randomly decide:
+//! You can define your own implementation to integrate your FTP(S) server with whatever
+//! authentication mechanism you need. For example, to define an `Authenticator` that will randomly
+//! decide:
 //!
 //! ```rust
 //! use rand::prelude::*;
-//! use libunftp::auth::Authenticator;
+//! use libunftp::auth::{Authenticator, UserDetail};
 //! use futures::Future;
 //! use async_trait::async_trait;
 //!
@@ -23,7 +24,16 @@
 //!     }
 //! }
 //!
+//! #[derive(Debug)]
 //! struct RandomUser;
+//!
+//! impl UserDetail for RandomUser {}
+//!
+//! impl std::fmt::Display for RandomUser {
+//!     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//!         write!(f, "RandomUser")
+//!     }
+//! }
 //! ```
 //! [`Server`]: ../server/struct.Server.html
 
