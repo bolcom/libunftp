4b5edc14be50ea48a61a99a606087f636c1a3e06
Hannes de Jager <hdejager@bol.com>
2020-04-30 23:25:35 +0200

    control_loop module

--- src/server/controlchan/mod.rs
+++ src/server/controlchan/mod.rs
@@ -5,6 +5,9 @@ use command::Command;
 
 pub(crate) mod handler;
 
+pub(crate) mod control_loop;
+pub(crate) use control_loop::{spawn_control_channel_loop, ControlParams};
+
 pub(super) mod commands;
 
 mod parse_error;
