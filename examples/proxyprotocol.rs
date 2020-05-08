1382781bf4a185acefbc7beecaecb36753566100
Hannes de Jager <hdejager@bol.com>
2020-05-08 11:10:38 +0200

    No IP needed to start proxy protocol mode (#210)

--- examples/proxyprotocol.rs
+++ examples/proxyprotocol.rs
@@ -5,9 +5,7 @@ pub async fn main() {
     pretty_env_logger::init();
 
     let addr = "127.0.0.1:2121";
-    let server = libunftp::Server::new_with_fs_root(std::env::temp_dir())
-        .proxy_protocol_mode("10.0.0.1", 2121)
-        .unwrap();
+    let server = libunftp::Server::new_with_fs_root(std::env::temp_dir()).proxy_protocol_mode(2121);
 
     info!("Starting ftp server with proxy protocol on {}", addr);
     server.listen(addr).await;
