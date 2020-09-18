b34394b31cfa6359251753c342a22e762c0de99b
Agoston Horvath <ahorvath@bol.com>
2020-09-18 19:29:24 +0200

    made gcs system test more organized

--- gcs_test.sh
+++
@@ -1,1 +0,0 @@
-docker run --rm --name fake-gcs --mount type=bind,src=`pwd`/data,dst=/data -p 9081:9081 -it fsouza/fake-gcs-server -scheme http -port 9081
--- tests/gcs.rs
+++ tests/gcs.rs
@@ -3,12 +3,50 @@ use pretty_assertions::assert_eq;
 use std::{str, time::Duration};
 use libunftp::storage::cloud_storage::CloudStorage;
 use libunftp::Server;
+use std::sync::Once;
+use std::process::Command;
+use tokio::time::delay_for;
+
+static INIT: Once = Once::new();
+
+pub fn initialize() {
+    INIT.call_once(|| {
+        let buf = std::env::current_dir().unwrap();
+        let current_dir = buf.display();
+
+        Command::new("docker")
+            .arg("stop")
+            .arg("fake-gcs")
+            .status()
+            .expect("docker failed");
+
+        Command::new("docker")
+            .arg("run")
+            .arg("-d")
+            .arg("--rm")
+            .arg("--name")
+            .arg("fake-gcs")
+            .arg("-v")
+            .arg(format!("{}/tests/resources/data:/data", current_dir))
+            .arg("-p")
+            .arg("9081:9081")
+            .arg("-it")
+            .arg("fsouza/fake-gcs-server")
+            .arg("-scheme")
+            .arg("http")
+            .arg("-port")
+            .arg("9081")
+            .status()
+            .expect("docker failed");
+    });
+}
 
 #[tokio::test]
 async fn mkdir() {
+    initialize();
     let addr: &str = "127.0.0.1:1234";
 
-    let service_account_key = yup_oauth2::read_service_account_key("gcs_sa_key.json").await.unwrap();
+    let service_account_key = yup_oauth2::read_service_account_key("tests/resources/gcs_sa_key.json").await.unwrap();
     tokio::spawn(Server::new(Box::new(move || {
         CloudStorage::new("http://localhost:9081", "test-bucket", service_account_key.clone())
     })).listen(addr));
