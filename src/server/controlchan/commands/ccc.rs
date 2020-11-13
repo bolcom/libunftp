29264a7c1f4a19245b23a12b33d2b19eab731fbf
Hannes de Jager <hdejager@bol.com>
2020-11-13 12:37:29 +0100

    CCC hangs the control channel. Disable it for now until properly implemented (#284)

--- src/server/controlchan/commands/ccc.rs
+++ src/server/controlchan/commands/ccc.rs
@@ -27,18 +27,19 @@ where
 {
     #[tracing_attributes::instrument]
     async fn handle(&self, args: CommandContext<S, U>) -> Result<Reply, ControlChanError> {
-        let mut tx: Sender<InternalMsg> = args.tx.clone();
-        let session = args.session.lock().await;
-        let logger = args.logger;
-        if session.cmd_tls {
-            tokio::spawn(async move {
-                if let Err(err) = tx.send(InternalMsg::PlaintextControlChannel).await {
-                    slog::warn!(logger, "{}", err);
-                }
-            });
-            Ok(Reply::new(ReplyCode::CommandOkay, "control channel in plaintext now"))
-        } else {
-            Ok(Reply::new(ReplyCode::Resp533, "control channel already in plaintext mode"))
-        }
+        // let mut tx: Sender<InternalMsg> = args.tx.clone();
+        // let session = args.session.lock().await;
+        // let logger = args.logger;
+        // if session.cmd_tls {
+        //     tokio::spawn(async move {
+        //         if let Err(err) = tx.send(InternalMsg::PlaintextControlChannel).await {
+        //             slog::warn!(logger, "{}", err);
+        //         }
+        //     });
+        //     Ok(Reply::new(ReplyCode::CommandOkay, "control channel in plaintext now"))
+        // } else {
+        //     Ok(Reply::new(ReplyCode::Resp533, "control channel already in plaintext mode"))
+        // }
+        Ok(Reply::new(ReplyCode::CommandNotImplemented, "CCC not implemented"))
     }
 }
