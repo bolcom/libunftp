506d22e1f3a04ac2df8ac44cf29140431aded4a2
Robby klein Gunnewiek <rkleingunnewiek@bol.com>
2021-05-01 23:03:49 +0200

    Pair programming result

--- crates/unftp-auth-jsonfile/src/lib.rs
+++ crates/unftp-auth-jsonfile/src/lib.rs
@@ -1,6 +1,80 @@
-//! [`Authenticator`] implementation that authenticates against a JSON file.
+//! This crate implements a libunftp `Authenticator` that authenticates against credentials in a JSON formatted file.
 //!
-//! [`Authenticator`]: libunftp::auth::Authenticator
+//! It supports both plaintext as well as [PBKDF2](https://tools.ietf.org/html/rfc2898#section-5.2) encoded passwords.
+//!
+//! # Plaintext example
+//!
+//! ```json
+//! [
+//!   {
+//!     "username": "alice",
+//!     "password": "I am in Wonderland!"
+//!   }
+//! ]
+//! ```
+//!
+//! # PBKDF2 encoded Example
+//!
+//! Both the salt and key need to be base64 encoded.
+//! Currently only HMAC_SHA256 is supported by libunftp (more will be supported later).
+//!
+//! There are various tools that can be used to generate the key.
+//! In this example, we use [nettle-pbkdf2](http://www.lysator.liu.se/~nisse/nettle/) which can generate the HMAC_SHA256.
+//!
+//! Generate a secure salt:
+//! ```sh
+//! salt=$(dd if=/dev/random bs=1 count=8)
+//! ```
+//!
+//! Generate the base64 encoded PBKDF2 key, to be copied into the `pbkdf2_key` field of the JSON structure.
+//! Make sure however to not exceed the output length of the digest algorithm (256 bit, 32 bytes in our case):
+//! ```sh
+//! echo -n "mypassword" | nettle-pbkdf2 -i 500000 -l 32 --hex-salt $(echo -n $salt | xxd -p -c 80) --raw |openssl base64 -A
+//! ```
+//!
+//! Convert the salt into base64 to be copied into the `pbkdf2_salt` field of the JSON structure:
+//! ```sh
+//! echo -n $salt | openssl base64 -A
+//! ```
+//!
+//! Now write these to the JSON file, as seen below. Make sure that `pbkdf2_iter` matches the iterations (`-i`) used with `nettle-pbkdf2`.
+//!
+//! ```json
+//! [
+//!   {
+//!     "username": "bob",
+//!     "pbkdf2_salt": "<<BASE_64_RANDOM_SALT>>",
+//!     "pbkdf2_key": "<<BASE_64_KEY>>",
+//!     "pbkdf2_iter": 500000
+//!   },
+//! ]
+//! ```
+//!
+//! # Mixed example
+//!
+//! It is possible to mix plaintext and pbkdf2 encoded type passwords.
+//!
+//! ```json
+//! [
+//!   {
+//!     "username": "alice",
+//!     "pbkdf2_salt": "<<BASE_64_RANDOM_SALT>>",
+//!     "pbkdf2_key": "<<BASE_64_KEY>>",
+//!     "pbkdf2_iter": 500000
+//!   },
+//!   {
+//!     "username": "bob",
+//!     "password": "This password is a joke"
+//!   }
+//! ]
+//! ```
+//!
+//! # Using it with libunftp
+//!
+//! Use [JsonFileAuthenticator::from_file](crate::JsonFileAuthenticator::from_file) to load the JSON structure directly from a file.
+//! See the example `examples/jsonfile_auth.rs`.
+//!
+//! Alternatively use another source for your JSON credentials, and use [JsonFileAuthenticator::from_json](crate::JsonFileAuthenticator::from_json) instead.
 
 use async_trait::async_trait;
 use bytes::Bytes;
@@ -29,43 +103,12 @@ enum Credentials {
     },
 }
 
-/// [`Authenticator`](libunftp::auth::Authenticator) implementation that authenticates against JSON.
+/// This structure implements the libunftp `Authenticator` trait
 ///
-/// Example of using nettle-pbkdf2 with a generated 256 bit secure salt
-///
-/// Generate a secure salt:
-/// salt=$(dd if=/dev/random bs=1 count=8)
-///
-/// Generate the base64 encoded PBKDF2 key, to be copied into the pbkdf2_key:
-/// echo -n "mypassword" | nettle-pbkdf2 -i 5000 -l 32 --hex-salt $(echo -n $salt | xxd -p -c 80) --raw |openssl base64 -A
-///
-/// Convert the salt into base64 to be copied into the pbkdf2_salt:
-/// echo -n $salt | openssl base64 -A
-///
-/// Verifies passwords against pbkdf2_key using the corresponding parameters form JSON.
-/// Example credentials file format:
-/// [
-//   {
-//     "username": "alice",
-//     "pbkdf2_salt": "<<BASE_64_RANDOM_SALT>>",
-//     "pbkdf2_key": "<<BASE_64_KEY>>",
-//     "pbkdf2_iter": 500000
-//   },
-//   {
-//     "username": "bob",
-//     "pbkdf2_salt": "<<BASE_64_RANDOM_SALT>>",
-//     "pbkdf2_key": "<<BASE_64_KEY>>",
-//     "pbkdf2_iter": 500000
-//   },
-//   {
-//     "username": "carol",
-//     "password": "secret"
-//   }
-// ]
 
 #[derive(Clone, Debug)]
 pub struct JsonFileAuthenticator {
-    db: HashMap<String, Password>,
+    credentials_map: HashMap<String, Password>,
 }
 
 #[derive(Clone, Debug)]
@@ -90,16 +133,13 @@ impl JsonFileAuthenticator {
 
     /// Initialize a new [`JsonFileAuthenticator`] from json string.
     pub fn from_json<T: Into<String>>(json: T) -> Result<Self, Box<dyn std::error::Error>> {
-        let db: Vec<Credentials> = serde_json::from_str::<Vec<Credentials>>(&json.into())?;
-        let map: Result<HashMap<String, Password>, _> = db
-            .into_iter()
-            .map(|user_info| JsonFileAuthenticator::creds_to_username_pw_pair(user_info))
-            .collect();
-        Ok(JsonFileAuthenticator { db: map? })
+        let credentials_list: Vec<Credentials> = serde_json::from_str::<Vec<Credentials>>(&json.into())?;
+        let map: Result<HashMap<String, Password>, _> = credentials_list.into_iter().map(Self::list_entry_to_map_entry).collect();
+        Ok(JsonFileAuthenticator { credentials_map: map? })
     }
 
-    fn creds_to_username_pw_pair(user_info: Credentials) -> Result<(String, Password), Box<dyn std::error::Error>> {
-        let r = match user_info {
+    fn list_entry_to_map_entry(user_info: Credentials) -> Result<(String, Password), Box<dyn std::error::Error>> {
+        let map_entry = match user_info {
             Credentials::Plaintext { username, password } => (username, Password::PlainPassword { password }),
             Credentials::Pbkdf2 {
                 username,
@@ -109,42 +149,51 @@ impl JsonFileAuthenticator {
             } => (
                 username.clone(),
                 Password::Pbkdf2Password {
-                    pbkdf2_salt: base64::decode(pbkdf2_salt)?.try_into()?,
-                    pbkdf2_key: base64::decode(pbkdf2_key)?
+                    pbkdf2_salt: base64::decode(pbkdf2_salt)
+                        .map_err(|_| "Could not base64 decode the salt")?
+                        .try_into()
+                        .map_err(|_| "Could not convert String to Bytes")?,
+                    pbkdf2_key: base64::decode(pbkdf2_key)
+                        .map_err(|_| "Could not decode base64")?
                         .validate("pbkdf2_key", &Length::Max(SHA256_OUTPUT_LEN))
-                        .result()?
-                        .unwrap()
-                        .try_into()?,
+                        .result()
+                        .map_err(|_| {
+                            format!("Key of user \"{}\" is too long", username)
+                        })?
+                        .unwrap() // this unwrap is just giving the value within
+                        .try_into()
+                        .map_err(|_| "Could not convert to Bytes")?,
                     pbkdf2_iter,
                 },
             ),
         };
-        Ok(r)
+        Ok(map_entry)
+    }
+
+    fn check_password(given_password: &str, actual_password: &Password) -> Result<(), ()> {
+        match actual_password {
+            Password::PlainPassword { password } => {
+                if password == given_password {
+                    Ok(())
+                } else {
+                    Err(())
+                }
+            }
+            Password::Pbkdf2Password {
+                pbkdf2_iter,
+                pbkdf2_salt,
+                pbkdf2_key,
+            } => verify(PBKDF2_HMAC_SHA256, *pbkdf2_iter, pbkdf2_salt, given_password.as_bytes(), pbkdf2_key).map_err(|_| ()),
+        }
     }
 }
 
 #[async_trait]
 impl Authenticator<DefaultUser> for JsonFileAuthenticator {
-    #[allow(clippy::type_complexity)]
     #[tracing_attributes::instrument]
-    async fn authenticate(&self, username: &str, pass: &str) -> Result<DefaultUser, AuthenticationError> {
-        let db: HashMap<String, Password> = self.db.clone();
-
-        if let Some(c) = db.get(username) {
-            if let Ok(()) = match c {
-                Password::PlainPassword { password } => {
-                    if pass == password {
-                        Ok(())
-                    } else {
-                        Err(())
-                    }
-                }
-                Password::Pbkdf2Password {
-                    pbkdf2_iter,
-                    pbkdf2_salt,
-                    pbkdf2_key,
-                } => verify(PBKDF2_HMAC_SHA256, *pbkdf2_iter, pbkdf2_salt, pass.as_bytes(), pbkdf2_key).map_err(|_| ()),
-            } {
+    async fn authenticate(&self, username: &str, given_password: &str) -> Result<DefaultUser, AuthenticationError> {
+        if let Some(actual_password) = self.credentials_map.get(username) {
+            if let Ok(()) = Self::check_password(given_password, actual_password) {
                 return Ok(DefaultUser);
             } else {
                 sleep(Duration::from_millis(1500)).await;
@@ -178,6 +227,10 @@ mod test {
   {
     "username": "carol",
     "password": "not so secure"
+  },
+  {
+    "username": "dan",
+    "password": ""
   }
 ]"#;
         let json_authenticator = JsonFileAuthenticator::from_json(json).unwrap();
@@ -196,6 +249,11 @@ mod test {
             DefaultUser
         );
         assert_eq!(json_authenticator.authenticate("carol", "not so secure").await.unwrap(), DefaultUser);
+        assert_eq!(json_authenticator.authenticate("dan", "").await.unwrap(), DefaultUser);
+        match json_authenticator.authenticate("carol", "this is the wrong password").await {
+            Err(AuthenticationError::BadPassword) => assert!(true),
+            _ => assert!(false),
+        }
         match json_authenticator.authenticate("bella", "this is the wrong password").await {
             Err(AuthenticationError::BadPassword) => assert!(true),
             _ => assert!(false),
