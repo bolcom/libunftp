3a999efbcf09654b6fa4c6755f64a578cb1f4951
Hannes de Jager <hdejager@bol.com>
2022-09-02 14:47:42 +0200

    Support EC private keys
    We didn't support elliptic curve private keys. See
    https://github.com/bolcom/unFTP/issues/126 .
    
    I took the opportunity to also improve the error handling in the TLS
    code.

--- src/server/tls.rs
+++ src/server/tls.rs
@@ -1,20 +1,19 @@
 use crate::options::{FtpsClientAuth, TlsFlags};
-use rustls::server::StoresServerSessions;
 use rustls::{
-    server::{AllowAnyAnonymousOrAuthenticatedClient, AllowAnyAuthenticatedClient, NoClientAuth, NoServerSessionStorage},
+    server::{AllowAnyAnonymousOrAuthenticatedClient, AllowAnyAuthenticatedClient, NoClientAuth, NoServerSessionStorage, StoresServerSessions},
     version::{TLS12, TLS13},
     Certificate, NoKeyLog, PrivateKey, RootCertStore, ServerConfig, SupportedProtocolVersion, Ticketer,
 };
 use std::{
-    error::Error,
-    fmt,
-    fmt::Formatter,
+    fmt::{self, Display, Formatter},
     fs::File,
-    io::BufReader,
+    io::{self, BufReader},
     path::{Path, PathBuf},
     sync::Arc,
     time::Duration,
 };
+use thiserror::Error;
+use tokio_rustls::webpki;
 
 // FTPSConfig shows how TLS security is configured for the server or a particular channel.
 #[derive(Clone)]
@@ -37,13 +36,30 @@ impl fmt::Debug for FtpsConfig {
 #[derive(Debug, Copy, Clone)]
 pub struct FtpsNotAvailable;
 
-impl fmt::Display for FtpsNotAvailable {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+impl Display for FtpsNotAvailable {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         write!(f, "FTPS not configured/available")
     }
 }
 
-impl Error for FtpsNotAvailable {}
+impl std::error::Error for FtpsNotAvailable {}
+
+// The error returned by new_config
+#[derive(Error, Debug)]
+#[error("TLS configuration error")]
+pub enum ConfigError {
+    #[error("found no private key")]
+    NoPrivateKey,
+
+    #[error("error reading key/cert input")]
+    Load(#[from] io::Error),
+
+    #[error("error building root certs")]
+    RootCerts(#[from] webpki::Error),
+
+    #[error("error initialising Rustls")]
+    RustlsInit(#[from] rustls::Error),
+}
 
 pub fn new_config<P: AsRef<Path>>(
     certs_file: P,
@@ -51,7 +67,7 @@ pub fn new_config<P: AsRef<Path>>(
     flags: TlsFlags,
     client_auth: FtpsClientAuth,
     trust_store: P,
-) -> std::io::Result<Arc<ServerConfig>> {
+) -> Result<Arc<ServerConfig>, ConfigError> {
     let certs: Vec<Certificate> = load_certs(certs_file)?;
     let privkey: PrivateKey = load_private_key(key_file)?;
 
@@ -78,10 +94,10 @@ pub fn new_config<P: AsRef<Path>>(
     let mut config = ServerConfig::builder()
         .with_safe_default_cipher_suites()
         .with_safe_default_kx_groups()
-        .with_protocol_versions(&versions).map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?
+        .with_protocol_versions(&versions).map_err(ConfigError::RustlsInit)?
         .with_client_cert_verifier(client_auther)
         // No SNI, single certificate 
-        .with_single_cert(certs, privkey).map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+        .with_single_cert(certs, privkey).map_err(ConfigError::RustlsInit)?;
 
     // Support session resumption with server side state (Session IDs)
     config.session_storage = if flags.contains(TlsFlags::RESUMPTION_SESS_ID) {
@@ -91,7 +107,7 @@ pub fn new_config<P: AsRef<Path>>(
     };
     // Support session resumption with tickets. See https://tools.ietf.org/html/rfc5077
     if flags.contains(TlsFlags::RESUMPTION_TICKETS) {
-        config.ticketer = Ticketer::new().map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+        config.ticketer = Ticketer::new().map_err(ConfigError::RustlsInit)?;
     };
     // Don't allow dumping session keys
     config.key_log = Arc::new(NoKeyLog {});
@@ -99,53 +115,45 @@ pub fn new_config<P: AsRef<Path>>(
     Ok(Arc::new(config))
 }
 
-fn root_cert_store<P: AsRef<Path>>(trust_pem: P) -> std::io::Result<RootCertStore> {
+fn root_cert_store<P: AsRef<Path>>(trust_pem: P) -> Result<RootCertStore, ConfigError> {
     let mut store = RootCertStore::empty();
     let certs = load_certs(trust_pem)?;
     for cert in certs.iter() {
-        store.add(cert).map_err(|_| std::io::Error::from(std::io::ErrorKind::Other))?
+        store.add(cert).map_err(ConfigError::RootCerts)?
     }
     Ok(store)
 }
 
-fn load_certs<P: AsRef<Path>>(filename: P) -> std::io::Result<Vec<Certificate>> {
+fn load_certs<P: AsRef<Path>>(filename: P) -> Result<Vec<Certificate>, ConfigError> {
     let certfile: File = File::open(filename)?;
     let mut reader: BufReader<File> = BufReader::new(certfile);
-    rustls_pemfile::certs(&mut reader)
-        .map_err(|_| std::io::Error::from(std::io::ErrorKind::Other))
-        .map(|v| {
-            let mut res = Vec::with_capacity(v.len());
-            for e in v {
-                res.push(Certificate(e));
-            }
-            res
-        })
+    rustls_pemfile::certs(&mut reader).map_err(ConfigError::Load).map(|v| {
+        let mut res = Vec::with_capacity(v.len());
+        for e in v {
+            res.push(Certificate(e));
+        }
+        res
+    })
 }
 
-fn load_private_key<P: AsRef<Path>>(filename: P) -> std::io::Result<PrivateKey> {
-    let rsa_keys = {
-        let keyfile = File::open(&filename)?;
-        let mut reader = BufReader::new(keyfile);
-        rustls_pemfile::rsa_private_keys(&mut reader).map_err(|_| std::io::Error::from(std::io::ErrorKind::Other))?
-    };
+fn load_private_key<P: AsRef<Path>>(filename: P) -> Result<PrivateKey, ConfigError> {
+    use rustls_pemfile::{read_one, Item};
+    use std::iter;
 
-    let pkcs8_keys = {
-        let keyfile = File::open(&filename)?;
-        let mut reader = BufReader::new(keyfile);
-        rustls_pemfile::pkcs8_private_keys(&mut reader).map_err(|_| std::io::Error::from(std::io::ErrorKind::Other))?
-    };
+    let keyfile = File::open(&filename)?;
+    let mut reader = BufReader::new(keyfile);
 
-    // prefer to load pkcs8 keys
-    let key = if !pkcs8_keys.is_empty() {
-        pkcs8_keys[0].clone()
-    } else {
-        if rsa_keys.is_empty() {
-            return Err(std::io::Error::from(std::io::ErrorKind::Other));
+    for item in iter::from_fn(|| read_one(&mut reader).transpose()) {
+        match item {
+            Ok(Item::RSAKey(key)) => return Ok(PrivateKey(key)),
+            Ok(Item::PKCS8Key(key)) => return Ok(PrivateKey(key)),
+            Ok(Item::ECKey(key)) => return Ok(PrivateKey(key)),
+            Err(e) => return Err(ConfigError::Load(e)),
+            _ => {}
         }
-        rsa_keys[0].clone()
-    };
+    }
 
-    Ok(PrivateKey(key))
+    Err(ConfigError::NoPrivateKey)
 }
 
 /// Stores the session IDs server side.
