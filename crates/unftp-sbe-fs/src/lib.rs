63e5ab944ad7be743249ab6c855e2b99677d7ba0
Alan Somers <asomers@gmail.com>
2023-08-07 15:23:50 -0600

    libunftp-sbe-fs: capability-mode compliance
    When a process runs in capability mode, as with Capsicum, it loses all
    access to global namespaces.  So for example, functions like open()
    don't work.  Instead, openat() may used.
    
    This commit replaces all global filesystem accesses in libunftp-sbe-fs
    with relative accesses, making it compliant with capability mode.
    
    The new cap_fs module is a 1:1 replacement for tokio::fs.  The code
    could be simplified, and some runtime allocations removed, by inlining
    this module into libunftp-sbe-fs::Filesystem.  However, I've left it
    separate for now to minimize diffs.

--- crates/unftp-sbe-fs/src/lib.rs
+++ crates/unftp-sbe-fs/src/lib.rs
@@ -20,23 +20,27 @@
 mod ext;
 pub use ext::ServerExt;
 
+mod cap_fs;
+
 use async_trait::async_trait;
 use cfg_if::cfg_if;
+use futures::{
+    future::TryFutureExt,
+    stream::TryStreamExt
+};
+use lazy_static::lazy_static;
 use libunftp::auth::UserDetail;
 use libunftp::storage::{Error, ErrorKind, Fileinfo, Metadata, Result, StorageBackend};
 use std::{
     fmt::Debug,
+    io::Seek,
     path::{Path, PathBuf},
+    sync::Arc,
     time::SystemTime,
 };
 
-cfg_if! {
-    if #[cfg(target_os = "linux")] {
-        use std::os::linux::fs::MetadataExt;
-    } else if #[cfg(target_os = "unix")] {
-        use std::os::unix::fs::MetadataExt;
-    }
-}
+#[cfg(target_os = "unix")]
+use std::os::unix::fs::MetadataExt;
 
 /// The Filesystem struct is an implementation of the StorageBackend trait that keeps its files
 /// inside a specific root directory on local disk.
@@ -44,23 +48,29 @@ cfg_if! {
 /// [`Filesystem`]: ./trait.Filesystem.html
 #[derive(Debug)]
 pub struct Filesystem {
-    root: PathBuf,
+    // The Arc is necessary so we can pass it to async closures.  Which is of dubious utility
+    // anyway, since most of those closures execute functions like fstatfs that are faster than the
+    // cost of switching a thread.
+    root_fd: Arc<cap_std::fs::Dir>
 }
 
 #[derive(Debug)]
 pub struct Meta {
-    inner: std::fs::Metadata,
+    inner: cap_std::fs::Metadata,
 }
 
-/// Returns the canonical path corresponding to the input path, sequences like '../' resolved.
-///
-/// I may decide to make this part of just the Filesystem implementation, because strictly speaking
-/// '../' is only special on the context of a filesystem. Then again, FTP does kind of imply a
-/// filesystem... hmm...
-fn canonicalize<P: AsRef<Path>>(path: P) -> Result<PathBuf> {
-    use path_abs::PathAbs;
-    let p = PathAbs::new(path).map_err(|_| Error::from(ErrorKind::FileNameNotAllowedError))?;
-    Ok(p.as_path().to_path_buf())
+/// Strip the "/" prefix, if any, from a path.  Suitable for preprocessing the input pathnames
+/// supplied by the FTP client.
+fn strip_prefixes(path: &Path) -> &Path {
+    lazy_static! {
+        static ref DOT: PathBuf = PathBuf::from(".");
+        static ref SLASH: PathBuf = PathBuf::from("/");
+    }
+    if path == SLASH.as_path() {
+        DOT.as_path()
+    } else {
+        path.strip_prefix("/").unwrap_or(path)
+    }
 }
 
 impl Filesystem {
@@ -69,31 +79,10 @@ impl Filesystem {
     /// asks for `hello.txt`, the server will send it `/srv/ftp/hello.txt`.
     pub fn new<P: Into<PathBuf>>(root: P) -> Self {
         let path = root.into();
+        let aa = cap_std::ambient_authority();
+        let root_fd = Arc::new(cap_std::fs::Dir::open_ambient_dir(&path, aa).unwrap());
         Filesystem {
-            root: canonicalize(&path).unwrap_or(path),
-        }
-    }
-
-    /// Returns the full, absolute and canonical path corresponding to the (relative to FTP root)
-    /// input path, resolving symlinks and sequences like '../'.
-    async fn full_path<P: AsRef<Path>>(&self, path: P) -> Result<PathBuf> {
-        // `path.join(other_path)` replaces `path` with `other_path` if `other_path` is absolute,
-        // so we have to check for it.
-        let path = path.as_ref();
-        let full_path = if path.starts_with("/") {
-            self.root.join(path.strip_prefix("/").unwrap())
-        } else {
-            self.root.join(path)
-        };
-
-        let real_full_path = tokio::task::spawn_blocking(move || canonicalize(full_path))
-            .await
-            .map_err(|e| Error::new(ErrorKind::LocalError, e))??;
-
-        if real_full_path.starts_with(&self.root) {
-            Ok(real_full_path)
-        } else {
-            Err(Error::from(ErrorKind::PermanentFileNotAvailable))
+            root_fd
         }
     }
 }
@@ -108,9 +97,8 @@ impl<User: UserDetail> StorageBackend<User> for Filesystem {
 
     #[tracing_attributes::instrument]
     async fn metadata<P: AsRef<Path> + Send + Debug>(&self, _user: &User, path: P) -> Result<Self::Metadata> {
-        let full_path = self.full_path(path).await?;
-
-        let fs_meta = tokio::fs::symlink_metadata(full_path)
+        let path = path.as_ref().to_owned();
+        let fs_meta = cap_fs::symlink_metadata(self.root_fd.clone(), &path)
             .await
             .map_err(|_| Error::from(ErrorKind::PermanentFileNotAvailable))?;
         Ok(Meta { inner: fs_meta })
@@ -123,35 +111,31 @@ impl<User: UserDetail> StorageBackend<User> for Filesystem {
         P: AsRef<Path> + Send + Debug,
         <Self as StorageBackend<User>>::Metadata: Metadata,
     {
-        let full_path: PathBuf = self.full_path(path).await?;
+        let path = strip_prefixes(path.as_ref());
 
-        let prefix: PathBuf = self.root.clone();
-
-        let mut rd: tokio::fs::ReadDir = tokio::fs::read_dir(full_path).await?;
-
-        let mut fis: Vec<Fileinfo<std::path::PathBuf, Self::Metadata>> = vec![];
-        while let Ok(Some(dir_entry)) = rd.next_entry().await {
-            let prefix = prefix.clone();
-            let path = dir_entry.path();
-            let relpath = path.strip_prefix(prefix).unwrap();
-            let relpath: PathBuf = std::path::PathBuf::from(relpath);
-            let metadata = tokio::fs::symlink_metadata(dir_entry.path()).await?;
-            let meta: Self::Metadata = Meta { inner: metadata };
-            fis.push(Fileinfo { path: relpath, metadata: meta })
-        }
+        let fis: Vec<Fileinfo<std::path::PathBuf, Self::Metadata>> =
+            cap_fs::read_dir(self.root_fd.clone(), path)
+            .and_then(|dirent| {
+                let entry_path: PathBuf = dirent.file_name().into();
+                cap_fs::symlink_metadata(self.root_fd.clone(), path.join(entry_path.clone()))
+                    .map_ok(move |meta| {
+                        let metadata = Meta { inner: meta };
+                        Fileinfo{ path: entry_path, metadata}
+                    })
+            }).try_collect::<Vec<_>>()
+            .await?;
 
         Ok(fis)
     }
 
     //#[tracing_attributes::instrument]
     async fn get<P: AsRef<Path> + Send + Debug>(&self, _user: &User, path: P, start_pos: u64) -> Result<Box<dyn tokio::io::AsyncRead + Send + Sync + Unpin>> {
-        use tokio::io::AsyncSeekExt;
-
-        let full_path = self.full_path(path).await?;
-        let mut file = tokio::fs::File::open(full_path).await?;
+        let path = path.as_ref().strip_prefix("/").unwrap_or(path.as_ref());
+        let mut file = cap_fs::open(self.root_fd.clone(), path).await?;
         if start_pos > 0 {
-            file.seek(std::io::SeekFrom::Start(start_pos)).await?;
+            file.seek(std::io::SeekFrom::Start(start_pos))?;
         }
+        let file = tokio::fs::File::from_std(file.into_std());
 
         Ok(Box::new(tokio::io::BufReader::with_capacity(4096, file)) as Box<dyn tokio::io::AsyncRead + Send + Sync + Unpin>)
     }
@@ -163,18 +147,16 @@ impl<User: UserDetail> StorageBackend<User> for Filesystem {
         path: P,
         start_pos: u64,
     ) -> Result<u64> {
-        use tokio::io::AsyncSeekExt;
         // TODO: Add permission checks
-        let path = path.as_ref();
-        let full_path = if path.starts_with("/") {
-            self.root.join(path.strip_prefix("/").unwrap())
-        } else {
-            self.root.join(path)
-        };
 
-        let mut file = tokio::fs::OpenOptions::new().write(true).create(true).open(full_path).await?;
-        file.set_len(start_pos).await?;
-        file.seek(std::io::SeekFrom::Start(start_pos)).await?;
+        let path = path.as_ref().strip_prefix("/").unwrap_or(path.as_ref());
+        let mut oo = cap_std::fs::OpenOptions::new();
+        oo.write(true).create(true);
+        let mut file = cap_fs::open_with(self.root_fd.clone(), path, oo).await?;
+        // XXX set_len ideally should go in a spawn_blocking
+        file.set_len(start_pos)?;
+        file.seek(std::io::SeekFrom::Start(start_pos))?;
+        let file = tokio::fs::File::from_std(file.into_std());
 
         let mut reader = tokio::io::BufReader::with_capacity(4096, bytes);
         let mut writer = tokio::io::BufWriter::with_capacity(4096, file);
@@ -185,35 +167,34 @@ impl<User: UserDetail> StorageBackend<User> for Filesystem {
 
     #[tracing_attributes::instrument]
     async fn del<P: AsRef<Path> + Send + Debug>(&self, _user: &User, path: P) -> Result<()> {
-        let full_path = self.full_path(path).await?;
-        tokio::fs::remove_file(full_path).await.map_err(|error: std::io::Error| error.into())
+        let path = path.as_ref().strip_prefix("/").unwrap_or(path.as_ref());
+        cap_fs::remove_file(self.root_fd.clone(), path).await.map_err(|error: std::io::Error| error.into())
     }
 
     #[tracing_attributes::instrument]
     async fn rmd<P: AsRef<Path> + Send + Debug>(&self, _user: &User, path: P) -> Result<()> {
-        let full_path = self.full_path(path).await?;
-        tokio::fs::remove_dir(full_path).await.map_err(|error: std::io::Error| error.into())
+        let path = path.as_ref().strip_prefix("/").unwrap_or(path.as_ref());
+        cap_fs::remove_dir(self.root_fd.clone(), path).await.map_err(|error: std::io::Error| error.into())
     }
 
     #[tracing_attributes::instrument]
     async fn mkd<P: AsRef<Path> + Send + Debug>(&self, _user: &User, path: P) -> Result<()> {
-        tokio::fs::create_dir(self.full_path(path).await?)
+        let path = path.as_ref().strip_prefix("/").unwrap_or(path.as_ref());
+        cap_fs::create_dir(self.root_fd.clone(), path)
             .await
             .map_err(|error: std::io::Error| error.into())
     }
 
     #[tracing_attributes::instrument]
     async fn rename<P: AsRef<Path> + Send + Debug>(&self, _user: &User, from: P, to: P) -> Result<()> {
-        let from = self.full_path(from).await?;
-        let to = self.full_path(to).await?;
+        let from = from.as_ref().strip_prefix("/").unwrap_or(from.as_ref());
+        let to = to.as_ref().strip_prefix("/").unwrap_or(to.as_ref());
 
-        let from_rename = from.clone();
-
-        let r = tokio::fs::symlink_metadata(from).await;
+        let r = cap_fs::symlink_metadata(self.root_fd.clone(), &from).await;
         match r {
             Ok(metadata) => {
                 if metadata.is_file() || metadata.is_dir() {
-                    let r = tokio::fs::rename(from_rename, to).await;
+                    let r = cap_fs::rename(self.root_fd.clone(), from, to).await;
                     match r {
                         Ok(_) => Ok(()),
                         Err(e) => Err(Error::new(ErrorKind::PermanentFileNotAvailable, e)),
@@ -227,9 +208,8 @@ impl<User: UserDetail> StorageBackend<User> for Filesystem {
     }
 
     #[tracing_attributes::instrument]
-    async fn cwd<P: AsRef<Path> + Send + Debug>(&self, _user: &User, path: P) -> Result<()> {
-        let full_path = self.full_path(path).await?;
-        tokio::fs::read_dir(full_path).await.map_err(|error: std::io::Error| error.into()).map(|_| ())
+    async fn cwd<P: AsRef<Path> + Send + Debug>(&self, user: &User, path: P) -> Result<()> {
+        self.list(user, path).await.map(drop)
     }
 }
 
@@ -251,15 +231,12 @@ impl Metadata for Meta {
     }
 
     fn modified(&self) -> Result<SystemTime> {
-        self.inner.modified().map_err(|e| e.into())
+        self.inner.modified().map(cap_std::time::SystemTime::into_std).map_err(|e| e.into())
     }
 
     fn gid(&self) -> u32 {
         cfg_if! {
-            if #[cfg(target_os = "linux")] {
-                self.inner.st_gid()
-            }
-            else if #[cfg(target_os = "unix")] {
+            if #[cfg(target_os = "unix")] {
                 self.inner.gid()
             } else {
                 0
@@ -269,10 +246,7 @@ impl Metadata for Meta {
 
     fn uid(&self) -> u32 {
         cfg_if! {
-            if #[cfg(target_os = "linux")] {
-                self.inner.st_uid()
-            }
-            else if #[cfg(target_os = "unix")] {
+            if #[cfg(target_os = "unix")] {
                 self.inner.uid()
             } else {
                 0
@@ -282,10 +256,7 @@ impl Metadata for Meta {
 
     fn links(&self) -> u64 {
         cfg_if! {
-            if #[cfg(target_os = "linux")] {
-                self.inner.st_nlink()
-            }
-            else if #[cfg(target_os = "unix")] {
+            if #[cfg(target_os = "unix")] {
                 self.inner.nlink()
             } else {
                 1
