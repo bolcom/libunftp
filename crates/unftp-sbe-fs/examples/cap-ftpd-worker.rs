7b683659156a098c1d1f2ebc43a670e381259e22
Alan Somers <asomers@gmail.com>
2023-08-23 16:31:05 -0600

    Add an example server that uses Capsicum

---
+++ crates/unftp-sbe-fs/examples/cap-ftpd-worker.rs
@@ -0,0 +1,55 @@
+// vim: tw=80
+//! A libexec helper for cap-std.  It takes an int as $1 which is interpreted as
+//! a file descriptor for an already-connected an authenticated control socket.
+//! Do not invoke this program directly.  Rather, invoke it by examples/cap-ftpd
+#![cfg(target_os = "freebsd")]
+
+use std::{
+    env,
+    os::fd::{FromRawFd, RawFd},
+    process::exit,
+    sync::{Arc, Mutex}
+};
+
+use tokio::net::TcpStream;
+
+use libunftp::Server;
+use unftp_sbe_fs::Filesystem;
+use unftp_auth_jsonfile::{JsonFileAuthenticator, User};
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    println!("Starting helper");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        eprintln!("Usage: {} <AUTH_FILE> <FD>", args[0]);
+        exit(2);
+    }
+    let fd: RawFd = if let Ok(fd) =  args[2].parse() {
+        fd
+    } else {
+        eprintln!("Usage: {} <FD>\nFD must be numeric", args[0]);
+        exit(2)
+    };
+
+    let std_stream = unsafe {
+        std::net::TcpStream::from_raw_fd(fd)
+    };
+
+    let control_sock = TcpStream::from_std(std_stream).unwrap();
+
+    let auth = Arc::new(JsonFileAuthenticator::from_file(args[1].clone()).unwrap());
+    // XXX This would be a lot easier if the libunftp API allowed creating the
+    // storage just before calling service.
+    let storage = Mutex::new(Some(Filesystem::new(std::env::temp_dir())));
+    let sgen = Box::new(move || storage.lock().unwrap().take().unwrap());
+    let server: Server<Filesystem, User> = libunftp::ServerBuilder::with_authenticator(
+        sgen, auth)
+        .pasv_listener(control_sock.local_addr().unwrap().ip())
+        .await
+        .build()
+        .unwrap();
+    capsicum::enter().unwrap();
+    server.service(control_sock).await.unwrap()
+}
---
+++ crates/unftp-sbe-fs/examples/cap-ftpd.rs
@@ -0,0 +1,30 @@
+//! A server that jails each connected session with Capsicum.
+#![cfg(target_os = "freebsd")]
+
+use std::{ffi::OsString, str::FromStr, path::Path};
+
+use unftp_sbe_fs::ServerExt;
+
+#[tokio::main(flavor = "current_thread")]
+pub async fn main() {
+    let addr = "127.0.0.1:2121";
+
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Usage: {} <AUTH_FILE.json>", args[0]);
+        std::process::exit(2);
+    }
+    let auth_file = &args[1];
+
+    let args: Vec<String> = std::env::args().collect();
+    let helper = Path::new(&args[0]).parent().unwrap().join("cap-ftpd-worker");
+    let helper_args = vec![OsString::from_str(auth_file).unwrap()];
+    let server = libunftp::Server::with_fs(std::env::temp_dir())
+        .connection_helper(helper.into(), helper_args)
+        .build()
+        .unwrap();
+
+    println!("Starting ftp server on {}", addr);
+    server.listen(addr).await.unwrap();
+}
