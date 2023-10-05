01baf1b755fd4ecc78440604e68b53cb2366ea5f
Alan Somers <asomers@gmail.com>
2023-10-05 14:39:05 -0600

    Move home directory logic into the cap-ftpd-worker example
    So that unftp-auth-jsonfile won't need to know it.

--- crates/unftp-sbe-fs/examples/cap-ftpd-worker.rs
+++ crates/unftp-sbe-fs/examples/cap-ftpd-worker.rs
@@ -13,8 +13,165 @@ use cfg_if::cfg_if;
 use tokio::net::TcpStream;
 
 use libunftp::Server;
-use unftp_auth_jsonfile::{JsonFileAuthenticator, User};
 use unftp_sbe_fs::Filesystem;
+
+mod auth {
+    use std::{
+        collections::HashMap,
+        fmt, fs,
+        io::Read,
+        path::{Path, PathBuf},
+        time::Duration,
+    };
+
+    use async_trait::async_trait;
+    use libunftp::auth::{AuthenticationError, Authenticator, DefaultUser, UserDetail};
+    use serde::Deserialize;
+    use tokio::time::sleep;
+
+    #[derive(Debug)]
+    pub struct User {
+        username: String,
+        home: Option<PathBuf>,
+    }
+
+    #[derive(Deserialize, Clone, Debug)]
+    #[serde(untagged)]
+    enum Credentials {
+        Plaintext {
+            username: String,
+            password: Option<String>,
+            home: Option<PathBuf>,
+        },
+    }
+
+    #[derive(Clone, Debug)]
+    struct UserCreds {
+        pub password: Option<String>,
+        pub home: Option<PathBuf>,
+    }
+
+    impl User {
+        fn new(username: &str, home: &Option<PathBuf>) -> Self {
+            User {
+                username: username.to_owned(),
+                home: home.clone(),
+            }
+        }
+    }
+
+    impl UserDetail for User {
+        fn home(&self) -> Option<&Path> {
+            match &self.home {
+                None => None,
+                Some(p) => Some(p.as_path()),
+            }
+        }
+    }
+
+    impl fmt::Display for User {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            f.write_str(self.username.as_str())
+        }
+    }
+
+    /// This structure implements the libunftp `Authenticator` trait
+    #[derive(Clone, Debug)]
+    pub struct JsonFileAuthenticator {
+        credentials_map: HashMap<String, UserCreds>,
+    }
+
+    impl JsonFileAuthenticator {
+        /// Initialize a new [`JsonFileAuthenticator`] from file.
+        pub fn from_file<P: AsRef<Path>>(filename: P) -> Result<Self, Box<dyn std::error::Error>> {
+            let mut f = fs::File::open(&filename)?;
+
+            let mut json = String::new();
+            f.read_to_string(&mut json)?;
+
+            Self::from_json(json)
+        }
+
+        /// Initialize a new [`JsonFileAuthenticator`] from json string.
+        pub fn from_json<T: Into<String>>(json: T) -> Result<Self, Box<dyn std::error::Error>> {
+            let credentials_list: Vec<Credentials> = serde_json::from_str::<Vec<Credentials>>(&json.into())?;
+            let map: Result<HashMap<String, UserCreds>, _> = credentials_list.into_iter().map(Self::list_entry_to_map_entry).collect();
+            Ok(JsonFileAuthenticator { credentials_map: map? })
+        }
+
+        fn list_entry_to_map_entry(user_info: Credentials) -> Result<(String, UserCreds), Box<dyn std::error::Error>> {
+            let map_entry = match user_info {
+                Credentials::Plaintext { username, password, home } => (username.clone(), UserCreds { password, home }),
+            };
+            Ok(map_entry)
+        }
+
+        fn check_password(given_password: &str, actual_password: &Option<String>) -> Result<(), ()> {
+            if let Some(pwd) = actual_password {
+                if pwd == given_password {
+                    Ok(())
+                } else {
+                    Err(())
+                }
+            } else {
+                Err(())
+            }
+        }
+    }
+
+    #[async_trait]
+    impl Authenticator<User> for JsonFileAuthenticator {
+        #[tracing_attributes::instrument]
+        async fn authenticate(&self, username: &str, creds: &libunftp::auth::Credentials) -> Result<User, AuthenticationError> {
+            let res = if let Some(actual_creds) = self.credentials_map.get(username) {
+                let pass_check_result = match &creds.password {
+                    Some(ref given_password) => {
+                        if Self::check_password(given_password, &actual_creds.password).is_ok() {
+                            Some(Ok(User::new(username, &actual_creds.home)))
+                        } else {
+                            Some(Err(AuthenticationError::BadPassword))
+                        }
+                    }
+                    None => None,
+                };
+
+                match pass_check_result {
+                    None => Err(AuthenticationError::BadPassword),
+                    Some(pass_res) => {
+                        if pass_res.is_ok() {
+                            Ok(User::new(username, &actual_creds.home))
+                        } else {
+                            pass_res
+                        }
+                    }
+                }
+            } else {
+                Err(AuthenticationError::BadUser)
+            };
+
+            if res.is_err() {
+                sleep(Duration::from_millis(1500)).await;
+            }
+
+            res
+        }
+
+        fn name(&self) -> &str {
+            std::any::type_name::<Self>()
+        }
+    }
+
+    #[async_trait]
+    impl Authenticator<DefaultUser> for JsonFileAuthenticator {
+        #[tracing_attributes::instrument]
+        async fn authenticate(&self, username: &str, creds: &libunftp::auth::Credentials) -> Result<DefaultUser, AuthenticationError> {
+            let _: User = self.authenticate(username, creds).await?;
+            Ok(DefaultUser {})
+        }
+    }
+}
+
+use auth::{JsonFileAuthenticator, User};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
